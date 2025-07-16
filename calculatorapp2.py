a911ed0e7d34c5a16ac658a1aadf467c1a2457f4
Nendirmwa Golen Peace <peacegolen4@gmail.com>
2025-07-16 13:19:50 +0000

    improved calculator app 5

--- calculatorapp2.py
+++ calculatorapp2.py
@@ -5,6 +5,10 @@ print("""1. Addition
 3. Multiplication"
 4. Division"
 """)
+
+
+
+#ADDITION
 #prompt user for input
 print("Enter two numbers to add")
 first_number=float(input("Enter first Number:"))
@@ -14,6 +18,12 @@ second_number=float(input("Enter Second Number:"))
 sum = first_number + second_number
 print(f"{first_number} + {second_number} = {sum}" 0)
 
+
+
+
+
+
+#SUBTRACTION
 #prompt user for input
 print("Enter two to perform subtraction")
 first_number=float(input("Enter first Number:"))
@@ -24,8 +34,13 @@ answer = first_number - second_number
 print(f"{first_number} + {second_number} = {answer} 0")
 
 
+
+
+
+
+#MULTIPLICATION
 #prompt user for input
-print("Enter two numbers to add")
+print("Enter two numbers to operate with")
 first_number=float(input("Enter first Number:"))
 second_number=float(input("Enter Second Number:"))
 
@@ -33,6 +48,11 @@ second_number=float(input("Enter Second Number:"))
 total = first_number * second_number
 print(f"{first_number} * {second_number} = {total}" 0)
 
+
+
+
+
+#DIVISION
 #prompt user for input
 print("Enter two numbers to add")
 first_number=float(input("Enter first Number:"))
@@ -41,5 +61,32 @@ second_number=float(input("Enter Second Number:"))
 #for Division operation
 total = first_number / second_number
 print(f"{first_number} / {second_number} = {total} 0")
+
+
+
+
+#EXPONENTIAL
+#prompt user for input
+print("Enter two numbers to operate with")
+first_number=float(input("Enter first Number:"))
+second_number=float(input("Enter Second Number:"))
+
+#for exponential operation 
+total = first_number ** second_number
+print(f"{first_number} ** {second_number} = {total} 0")
+
+
+
+
+
+#FLOOR DIVISION
+#prompt user for input 
+print("Enter two numbers to add")
+first_number=float(input("Enter first Number:"))
+second_number=float(input("Enter Second Number:"))
+
+#forFloor Division operation
+total = first_number // second_number
+print(f"{first_number} // {second_number} = {total} 0")
 
 
